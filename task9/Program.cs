280f3827f354cde829cd014a557ad774a26ab991
sweetie <ivan.linzov@gmail.com>
2022-08-08 21:10:58 +0300

    task9 was realized

--- task9/Program.cs
+++ task9/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 9. Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+// 78 -> 8 
+// 12-> 2 
+// 85 -> 8
+
+int number = new Random().Next(10, 100);
+Console.WriteLine($"Наше случайное {number} число");
+int digit2 = number % 10;
+int digit1 = number / 10;
+if (digit1 > digit2)
+{
+    Console.Write(digit1);
+}
+else if (digit2 > digit1)
+{
+    Console.Write(digit2);
+}
+else
+{
+    Console.Write("Цифры равны");
+}
+
+
+
+
